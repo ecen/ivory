bfd42466db0e68a443a3458a0f4e5d1eaafe6d7d
ecen <ericgul@student.chalmers.se>
2017-09-18 11:24:04 +0200

    New task.

---
+++ mve055_statistics/7-49.py
@@ -0,0 +1,29 @@
+import math
+
+d1 = [3,2,2,2,2,3,3,3,
+      3,3,3,3,4,3,2,3,
+      3,2,3,3,3,3,3,1,
+      3,3,3,3,3,3,3,3,
+      3,3,2,3,3,3,3,3]
+
+def mean(data):
+	sum = 0
+	for i in data:
+		sum += i;
+	return sum / len(data)
+
+def variance(data):
+	m = mean(data)
+	s2 = 0
+	n = len(data)
+	for i in range(0, n):
+		s2 += ((data[i] - m)**2)/(n-1)
+	return s2
+	
+def stdDeviation(data):
+	return math.sqrt(variance(data))
+
+print("Mean: " + str(mean(d1)))
+print("Var: " + str(variance(d1)))
+print("Standard Deviation: " + str(stdDeviation(d1)))
+#print(sorted(d1))
